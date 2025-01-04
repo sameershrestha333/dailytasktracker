f38b37673e5225139dd492caadbb0d3f371c124a
sameershrestha333 <sameershrestha333@gmail.com>
2025-01-03 20:43:26 -0600

    Add datecreated filed in task class where i will tracked the datecreated

--- src/main/java/com/dailytasktracker/model/Task.java
+++ src/main/java/com/dailytasktracker/model/Task.java
@@ -1,10 +1,13 @@
 package com.dailytasktracker.model;
 
 import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
 
 import javax.persistence.*;
+import java.time.LocalDateTime;
 import java.util.Optional;
 
 @Entity
@@ -22,6 +25,10 @@ public class Task {
     @JoinColumn(name = "account_id", nullable = false)
     @JsonBackReference  // Prevent recursion during serialization of Task -> Account
     private Account account;
+
+    @CreationTimestamp
+    @JsonFormat(pattern = "yyyy:MM:dd hh a")  // 'hh' for 12-hour format, 'a' for AM/PM
+    private LocalDateTime dateCreated;
 
     public Task(String description, Account account) {
         this.description = description;
